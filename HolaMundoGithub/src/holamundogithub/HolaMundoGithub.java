0154bec2580f0a03ce0cc481d5776b7f7b527188
a0y3502731y <a0y3502731y@S09-PC16>
2021-11-22 19:24:45 +0100

    Creado el proyecto y añadido print Hello world

---
+++ HolaMundoGithub/src/holamundogithub/HolaMundoGithub.java
@@ -0,0 +1,24 @@
+/*
+ * Proyecto HolaMundoGithub - Archivo HolaMundoGithub.java - Compañía DAW
+ * Licencia Creative Commons BY-NC-SA 4.0
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
+package holamundogithub;
+
+/**
+ * @author Lucas Ezequiel Gyori <legyori@gmail.com> 
+ * @company DAW
+ * @version 1.0
+ * @date 22 nov. 2021 19:06:18
+ */
+public class HolaMundoGithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
