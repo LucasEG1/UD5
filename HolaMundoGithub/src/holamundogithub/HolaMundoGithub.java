bd57f9624227f702320ed00efbd2b6235d8ddfc4
Lucas <legyori@gmail.com>
2021-11-22 19:49:21 +0100

    Eliminado comentario Añadido espaciado

--- HolaMundoGithub/src/holamundogithub/HolaMundoGithub.java
+++ HolaMundoGithub/src/holamundogithub/HolaMundoGithub.java
@@ -5,10 +5,7 @@ package holamundogithub;
 import java.util.Scanner;
 
 public class HolaMundoGithub {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         
