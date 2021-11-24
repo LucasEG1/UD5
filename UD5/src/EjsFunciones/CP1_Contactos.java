a44e2b3426acb22efcb6b751604af3b48a8f9368
Lucas <legyori@gmail.com>
2021-11-24 21:41:33 +0100

    Creación de CP-1 (contactos)

---
+++ UD5/src/EjsFunciones/CP1_Contactos.java
@@ -0,0 +1,26 @@
+
+package EjsFunciones;
+
+import java.util.Scanner;
+
+public class CP1_Contactos {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        int a = leer.nextInt();
+        System.out.println(a);
+    }
+}
