da1ab598d77773ec31666ed237b152b94edee5d1
Lucas <legyori@gmail.com>
2021-11-25 20:04:31 +0100

    Creación de estructura de las funciones (solo comments)

--- UD5/src/EjsFunciones/CP1_Contactos.java
+++ UD5/src/EjsFunciones/CP1_Contactos.java
@@ -5,22 +5,26 @@ import java.util.Scanner;
 
 public class CP1_Contactos {
     
+    //Mostrar menú
+
+    //Ver contactos
     
+    //Agregar contacto
     
+    //Eliminar contacto
     
+    //Buscar por nombre
     
+    //Buscar por teléfono
     
+    //Buscar por correo
     
+    //búsqueda global
     
-    
-    
-    
-    
+    //Salir
     
     
     public static void main(String[] args) {
-        Scanner leer = new Scanner(System.in);
-        int a = leer.nextInt();
-        System.out.println(a);
+        
     }
 }
