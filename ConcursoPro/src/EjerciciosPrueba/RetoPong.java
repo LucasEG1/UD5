a58fdee289bc17a721ffeaa76bbe8eccd42ba7b7
Lucas <legyori@gmail.com>
2022-01-20 20:49:24 +0100

    Reto PIC PIC PIC PONG! (PrograMaMe 2021)

---
+++ ConcursoPro/src/EjerciciosPrueba/RetoPong.java
@@ -0,0 +1,41 @@
+package EjerciciosPrueba;
+
+import java.util.Scanner;
+
+public class RetoPong {
+
+    public static void main(String[] args) {
+
+        Scanner leer = new Scanner(System.in);
+        int sonidos = 1;
+        String jugada;
+
+        while (sonidos != 0) {
+            int contadorPIC = 0;
+            int puntosI = 0;
+            int puntosD = 0;
+            sonidos = leer.nextInt();
+
+            if (sonidos != 0) {
+                for (int i = 0; i < sonidos; i++) {
+                    jugada = leer.next();
+
+                    switch (jugada) {
+
+                        case "PIC":
+                            contadorPIC++;
+                            break;
+
+                        case "PONG!":
+                            if (contadorPIC % 2 == 0) {
+                                puntosI++;
+                            } else {
+                                puntosD++;
+                            }
+                    }
+                }
+                System.out.println(puntosI + " " + puntosD);
+            }
+        }
+    }
+}
